af86a868702f77d238fb4480e8fc1efbc942b49b
Flary <gaboh6464@gmail.com>
2018-12-08 15:26:07 -0500

    Made kick command functional
    2018/12/08 15:26

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -2,6 +2,8 @@ const {
   Command
 } = require('discord.js-commando');
 
+const Discord = require('discord.js');
+
 module.exports = class ReplyCommand extends Command {
   constructor(client) {
     super(client, {
@@ -19,13 +21,88 @@ module.exports = class ReplyCommand extends Command {
         {
             key: 'member',
             prompt: 'Who would you like to kick from the server?',
-            type: 'member'
+            type: 'member',
+            /*validate: member => {
+              // If bot can kick member
+              console.log(member)
+              console.log(member.kickable)
+              if (member.kickable) return true;
+              // If can't, return message
+              return "Sorry, but I can't kick the member due to missing permissions or too low of a role.";
+            }*/
+        },
+        {
+            key: 'reason',
+            prompt: 'Why will the member be kicked?',
+            type: 'string',
+            default: 'No reason set!'
         }
     ]
     });
   }
 
   run(msg, args) {
-    return msg.say('You wanted to kick: ' + args.member);
+    const member = args.member;
+    const user = args.member.user;
+
+    const discordReason = `${args.reason} | Executed by ${msg.author} / ${msg.author.tag}`;
+
+    args.member.kick(discordReason);
+
+    const embed = new Discord.RichEmbed()
+
+      // Sets basic info for the embed
+      .setTitle(`${user.tag} has been kicked from the server.`)
+      .setThumbnail(user.avatarURL)
+
+      // Sets the colour to the bots own roles colour
+      .setColor(msg.guild.me.displayColor)
+
+      // Let's start adding some fields.
+
+      .addField(
+        "Name",
+        user.username,
+        true
+      )
+
+      .addField(
+        "Nick",
+        member.displayName,
+        true
+      )
+
+      .addField(
+        "ID",
+        user.id,
+        true
+      )
+
+      .addField(
+        "Bot",
+        user.bot,
+        true
+      )
+
+      .addField(
+        "Status",
+        member.presence.status,
+        true
+      )
+
+      .addField(
+        "Command executer",
+        `${msg.author} / ${msg.author.tag}`,
+        true
+      )
+
+      .addField(
+        "Reason for kick",
+        `${args.reason}`,
+        true
+      )
+
+    // Finally sends the message
+    msg.embed(embed);
   }
 };
